9d5fa5e9cb6e50ae9ebfc16d1a692e0577da967a
EldarBruveris <eldar.bruverys@innowise.com>
2024-10-30 15:17:18 +0300

    quick sort

--- public/index.php
+++ public/index.php
@@ -6,5 +6,13 @@ use App\Index;
 
 require 'vendor/autoload.php';
 
+
+$arr = [];
+for($i = 0; $i<9; $i++){
+    array_push($arr, rand(0, 200));
+}
+$array = [5, 2, 9, 15, 7];
 $a = new Index();
-$a->test();
+var_dump (Index::quickSort($arr));
+
+
--- src/Index.php
+++ src/Index.php
@@ -6,8 +6,29 @@ namespace App;
 
 final class Index
 {
-    public function test(): void
+    public function test()
     {
-        echo 'Hello' . PHP_EOL;
+        echo "<br><br>";
     }
+
+    public static function quickSort(array $array): array {
+        if (count($array) < 2) {
+            return $array;
+        }
+    
+        $pivot = $array[0];
+        $left = [];
+        $right = [];
+    
+        for ($i = 1; $i < count($array); $i++) {
+            if ($array[$i] < $pivot) {
+                $left[] = $array[$i];
+            } else {
+                $right[] = $array[$i];
+            }
+        }
+    
+        return array_merge(self::quickSort($left), [$pivot], self::quickSort($right));
+    }
+
 }
