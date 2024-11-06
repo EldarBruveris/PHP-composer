01de72d121fba477d1658ba4647247a352771df7
EldarBruveris <eldar.bruverys@innowise.com>
2024-11-06 23:25:43 +0300

    Second task fix

--- Frequency/task2.php
+++ Frequency/task2.php
@@ -4,24 +4,20 @@
 
 class Solution {
 
-/**
- * @param Integer[] $nums
- * @param Integer $k
- * @return Integer[]
- */
+
 function topKFrequent($nums, $k) {
-$frequencyMap = [];
+    $frequencyMap = [];
 
-foreach ($nums as $num) {
-    if (isset($frequencyMap[$num])) {
-        $frequencyMap[$num]++;
-    } else {
-        $frequencyMap[$num] = 1;
+    foreach ($nums as $num) {
+        if (isset($frequencyMap[$num])) {
+            $frequencyMap[$num]++;
+        } else {
+            $frequencyMap[$num] = 1;
+        }
     }
-}
 
-arsort($frequencyMap);
-return array_slice(array_keys($frequencyMap), 0, $k);
-}
+    arsort($frequencyMap);
+    return array_slice(array_keys($frequencyMap), 0, $k);
+    }
 
 }
